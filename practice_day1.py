555c778e347fbe84c10d92a7919ea279b9714b3d
sownak <sownakchaudhury@gmail.com>
2026-08-11 18:22:01 +0530

    Added Python practice

---
+++ practice_day1.py
@@ -0,0 +1,21 @@
+# Student Details and Data Types Practice
+
+# Variables
+student_name = "Sownak Chaudhury"
+age = 18
+cgpa = 8.5
+is_enrolled = True
+
+# Printing Values
+print("--- Student Information ---")
+print("Name:", student_name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Enrolled Status:", is_enrolled)
+
+# Checking and Printing Data Types
+print("\n--- Data Types Check ---")
+print("Type of student_name:", type(student_name))
+print("Type of age:", type(age))
+print("Type of cgpa:", type(cgpa))
+print("Type of is_enrolled:", type(is_enrolled))
