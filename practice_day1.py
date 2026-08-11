fb45d8416114b43b7be67d2e6a924508a42d5854
sownak <sownakchaudhury@gmail.com>
2026-08-11 19:11:27 +0530

    Updated code with BTech and CSE AI&ML

--- practice_day1.py
+++ practice_day1.py
@@ -1,21 +1,27 @@
 # Student Details and Data Types Practice
 
 # Variables
-student_name = "Sownak Chaudhury"
-age = 18
-cgpa = 8.5
-is_enrolled = True
+student_name = "Sownak Chaudhury"                   # String
+degree = "B.Tech"                                   # String
+department = "CSE (AI & ML)"                        # String
+age = 18                                            # Integer
+height = 5.8                                        # Float
+is_student = True                                   # Boolean
 
 # Printing Values
 print("--- Student Information ---")
 print("Name:", student_name)
+print("Degree:", degree)
+print("Department:", department)
 print("Age:", age)
-print("CGPA:", cgpa)
-print("Enrolled Status:", is_enrolled)
+print("Height:", height)
+print("Is Student:", is_student)
 
 # Checking and Printing Data Types
 print("\n--- Data Types Check ---")
 print("Type of student_name:", type(student_name))
+print("Type of degree:", type(degree))
+print("Type of department:", type(department))
 print("Type of age:", type(age))
-print("Type of cgpa:", type(cgpa))
-print("Type of is_enrolled:", type(is_enrolled))
+print("Type of height:", type(height))
+print("Type of is_student:", type(is_student))
